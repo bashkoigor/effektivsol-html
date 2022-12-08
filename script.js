e9d7d0438b8ff4f9b70b154d83691354b498a6d3
admin <admin@mail.loc>
2022-12-08 19:01:21 +0200

    init main content

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 $(".dropdown-list .dropdown .title").click(function () {
     $(this).parent().toggleClass("closed");
 });
+
+const obRecommended1 = document.querySelector("#recommended1");
+if (obRecommended1) {
+    const recommended1 = new Carousel(obRecommended1, {
+        slidesPerPage: 1,
+        center: false
+    });
+}
