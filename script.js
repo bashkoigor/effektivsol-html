7c594121e305bb32a92cfe0f1a8e69ad3b5c0feb
admin <admin@mail.loc>
2022-12-16 18:56:56 +0200

    product page v3

--- script.js
+++ script.js
@@ -3,10 +3,30 @@ $(".dropdown-list .dropdown .title").click(function () {
     $(this).parent().toggleClass("closed");
 });
 
+// Recommended products Carousel
 const obRecommended1 = document.querySelector("#recommended1");
 if (obRecommended1) {
     const recommended1 = new Carousel(obRecommended1, {
         slidesPerPage: 1,
         center: false
+    });
+}
+
+// For product page
+if (document.querySelector(".product")) {
+    // Product Carousel
+    const productCarousel = new Carousel(document.querySelector("#product-carousel"), {
+        Dots: false,
+    });
+    const thumbCarousel = new Carousel(document.querySelector("#product-thumb-carousel"), {
+        Sync: {
+            target: productCarousel,
+            friction: 0,
+        },
+        Dots: false,
+        Navigation: false,
+        center: true,
+        slidesPerPage: 1,
+        infinite: false,
     });
 }
